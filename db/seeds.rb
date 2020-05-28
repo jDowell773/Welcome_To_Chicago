ae091bec133836913b882307ab04251cea18483e
JonathanDowell <mr.jonathandowell@gmail.com>
2020-05-28 10:51:24 -0500

    made some fake data in seeds file & tested show actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Restaurants 
+
+Restaurant.create(name: "Catch 35", location: "Wacker drive", image_url: "", description: "upscale seafood restaurant located on Wacker drive in Chicago")
+
+Restaurant.create(name: "Gino's East", location: "downtown", image_url: "", description: "Italian style pizzeria located in dowwntown Chicago")
+
+
+# Museums
+
+Museum.create(name: "Science and Industry", location: "southside", image_url: "", description: "museum located on the southside of Chicago")
+
+Museum.create(name: "Field Museum", location: "downtown", image_url: "", description: "museum located just south of the downtown are in Chicago")
