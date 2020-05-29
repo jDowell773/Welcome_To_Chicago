c1ddf65991aa0e4abfa8e569812185430e835dd7
JonathanDowell <mr.jonathandowell@gmail.com>
2020-05-29 12:42:51 -0500

    set up all routes for bars controller

--- app/controllers/api/bars_controller.rb
+++ app/controllers/api/bars_controller.rb
@@ -1,2 +1,20 @@
 class Api::BarsController < ApplicationController
+
+  def index
+    @bars = Bar.all 
+    render 'index.json.jb' 
+  end
+
+  def show
+    @bar = Bar.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def destroy
+    bar = Bar.find_by(id: params[:id])
+    bar.destroy 
+    render json: {message: "Bar successfully destroyed"}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
    namespace :api do
+    
+    get "/bars" => "bars#index"
+    get "/bars/:id" => "bars#show"
+    delete "/bars/:id" => "bars#destroy"
 
-     get "/restaurants" => "restaurants#index"
-     get "/restaurants/:id" => "restaurants#show"
-     delete "/restaurants/:id" => "restaurants#destroy"
 
-     get "/museums" => "museums#index"
-     get "/museums/:id" => "museums#show"
-     delete "/museums/:id" => "museums#destroy"
+    get "/restaurants" => "restaurants#index"
+    get "/restaurants/:id" => "restaurants#show"
+    delete "/restaurants/:id" => "restaurants#destroy"
 
-     post "/users" => "users#create"
-     get "/users/current" => "users#current"
+    get "/museums" => "museums#index"
+    get "/museums/:id" => "museums#show"
+    delete "/museums/:id" => "museums#destroy"
 
-     post "/sessions" => "sessions#create"
+    post "/users" => "users#create"
+    get "/users/current" => "users#current"
+
+    post "/sessions" => "sessions#create"
 
    end
 end
